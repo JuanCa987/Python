c0e96e7b7790c315e71b46cb24711929830a67e7
JuanCa987 <juan.ausecha@misena.edu.co>
2024-10-24 19:07:37 -0500

    sobre fucniones

---
+++ fucntion.py
@@ -0,0 +1,8 @@
+"""function: es una declaracion que se ejecutara cuando se llame a la funcion """
+
+def hello(first_name,last_name,age):
+    print("hi "+first_name+" "+last_name)
+    print("you are "+str(age)+"  years old")
+    print("Have a good day")
+
+hello("Juan","Ause",20)
